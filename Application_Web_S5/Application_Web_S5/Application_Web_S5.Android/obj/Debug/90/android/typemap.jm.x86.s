3db0d5f09e2a31e430bd131dea2688c32be195af
Mathieu <math_tgod@hotmail.com>
2019-08-19 14:43:46 -0400

    first commit

---
+++ Application_Web_S5/Application_Web_S5/Application_Web_S5.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 741e0cfa6169f460c0c1cc8d7ed8a7c4cc06e777 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1343
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 351867
+	.include	"typemap.jm.inc"
